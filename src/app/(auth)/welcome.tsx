ea624dd3519d726e770af9b87bf274f31c92c01b
mardoqueudickson1 <mardoqueudrums1999@gmail.com>
2025-01-21 00:02:07 +0100

    feat: swipe screen added

---
+++ src/app/(auth)/welcome.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { onboarding } from "@/constants";
+
+const Onbording = () => {
+  const swiperRef = React.useRef(null);
+  const [ActiveiIndex, setActiveiIndex] = useState(0);
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => router.replace("/(auth)/sign-up")}
+        className="w-full flex items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[35px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[35px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveiIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View>
+            <Text> {item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
+    </SafeAreaView>
+  );
+};
+
+
+export default Onbording;
